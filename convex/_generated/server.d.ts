b6a6be83c7999844642c659fec20a64d6592db34
ybsk00 <ybsk00@naver.com>
2026-02-21 09:38:55 +0900

    Add convex generated type stubs to unblock Vercel build

---
+++ convex/_generated/server.d.ts
@@ -0,0 +1,6 @@
+export const mutation = {} as any;
+export const query = {} as any;
+export const action = {} as any;
+export const internalMutation = {} as any;
+export const internalQuery = {} as any;
+export const internalAction = {} as any;
